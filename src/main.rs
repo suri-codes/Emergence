8045584f0a5a013318f46260baf67c7ec500cfe9
Surendra Jammishetti <suri312006@gmail.com>
2025-08-03 13:53:16 -0700

    fixed toolchain

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Emergence",
         native_options,
         Box::new(|cc| Ok(Box::new(Emergence::TemplateApp::new(cc)))),
     )
